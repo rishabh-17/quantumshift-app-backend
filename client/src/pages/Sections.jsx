0b20c3e8e4a4b3d5b46dc6d1b5b364eeb45f891d
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-04-02 22:53:54 +0530

    firebase setup

--- client/src/pages/Sections.jsx
+++ client/src/pages/Sections.jsx
@@ -3,25 +3,52 @@ import "quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
 import axios from "axios";
 import ImageUploading from "react-images-uploading";
+import firebase from "firebase/app";
+import "firebase/storage";
 
 const CreateBlogPage = () => {
-  const [blogData, setBlogData] = useState({
+  const [sectionData, setSectionData] = useState({
     title: "",
-    blog: "",
-    thumbnail: "",
   });
   const [images, setImages] = useState([]);
+  const [audio, setAudios] = useState([]);
+  const [video, setVideos] = useState([]);
+
+  const firebaseConfig = {
+    apiKey: "import.meta.env.VITE_API_KEY",
+    authDomain: "import.meta.env.VITE_AUTH_DOMAIN",
+    projectId: "import.meta.env.VITE_PROJECT_ID",
+    storageBucket: "import.meta.env.VITE_STORAGE_BUCKET",
+    messagingSenderId: "import.meta.env.VITE_MESSAGING_SENDER_ID",
+    appId: "import.meta.env.VITE_APP_ID",
+  };
 
   const handleChange = (e) => {
-    setBlogData({
-      ...blogData,
+    setSectionData({
+      ...sectionData,
       [e.target.name]: e.target.value,
     });
   };
 
+  const uploadAudioToFirebase = (audioFile) => {
+    const storageRef = firebase.storage().ref();
+    const audioRef = storageRef.child(`audio/${audioFile.name}`);
+
+    return audioRef
+      .put(audioFile)
+      .then((snapshot) => {
+        console.log("Audio uploaded successfully");
+        return snapshot.ref.getDownloadURL();
+      })
+      .catch((error) => {
+        console.error("Error uploading audio:", error);
+        return null;
+      });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(blogData);
+    console.log(sectionData);
   };
 
   const maxNumber = 1;
@@ -105,28 +132,51 @@ const CreateBlogPage = () => {
     setContent(content);
   };
 
-  const handleSave = () => {
-    if (content && images?.[0]) {
-      axios
-        .post("/api/v1/blog/create", {
-          blog: content,
-          thumbnail: images?.[0],
-          title: title,
-          author: author,
-        })
-        .then(() => {
-          alert("Success");
-        })
-        .catch((err) => {
-          alert("Error: ");
-        });
+  const uploadFileToFirebase = async (file) => {
+    try {
+      const storageRef = firebase.storage().ref();
+      const audioRef = storageRef.child(`audio/${file.name}`);
+
+      const snapshot = await audioRef.put(file);
+
+      const downloadURL = await snapshot.ref.getDownloadURL();
+
+      return downloadURL;
+    } catch (error) {
+      console.error("Error uploading audio file:", error);
+      throw error;
+    }
+  };
+
+  const handleSave = async () => {
+    firebase.initializeApp(firebaseConfig);
+    const files = e.target.files;
+    const uploadPromises = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      uploadPromises.push(uploadAudioFile(file));
+    }
+
+    try {
+      const downloadURLs = await Promise.all(uploadPromises);
+      console.log("Upload completed:", downloadURLs);
+    } catch (error) {
+      console.error("Error uploading audio files:", error);
     }
   };
 
   const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
     console.log(imageList, addUpdateIndex);
     setImages(imageList);
+  };
+
+  const handleAudioUpload = (e) => {
+    const files = e.target.files;
+  };
+
+  const handleVideoUpload = (e) => {
+    const file = e.target.files[0];
   };
 
   return (
@@ -141,26 +191,13 @@ const CreateBlogPage = () => {
             type="text"
             id="title"
             name="title"
-            value={blogData.title}
+            value={sectionData.title}
             onChange={handleChange}
             className="border  bg-gray-100 border-gray-300 rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
-        <div className="mb-8">
-          <label htmlFor="blog" className="font-bold block mb-2">
-            Blog:
-          </label>
-          <ReactQuill
-            theme="snow"
-            modules={modules}
-            formats={formats}
-            placeholder="Write your content..."
-            onChange={handleProcedureContentChange}
-            style={{ height: "400px" }}
-          />
-        </div>
 
-        <div className="mb-8 mt-24">
+        <div className="my-8">
           <h2 className="font-bold mb-2">Upload Thumbnail</h2>
           <ImageUploading
             value={images}
@@ -224,6 +261,61 @@ const CreateBlogPage = () => {
           </ImageUploading>
         </div>
 
+        <div className="my-8">
+          <label className="block mb-2" for="file_input">
+            <h2 className="font-bold mb-2">Upload Video</h2>
+          </label>
+          <input
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="file_input"
+            type="file"
+            onChange={handleVideoUpload}
+          />
+        </div>
+
+        <div className="my-8">
+          <label className="block mb-2" for="file_input">
+            <h2 className="font-bold mb-2">Upload Audio Files</h2>
+          </label>
+          <div className="flex items-center justify-center w-full">
+            <label
+              for="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  Audio Files
+                </p>
+              </div>
+              <input
+                id="dropzone-file"
+                type="file"
+                multiple
+                className="hidden"
+                onChange={handleAudioUpload}
+              />
+            </label>
+          </div>
+        </div>
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
